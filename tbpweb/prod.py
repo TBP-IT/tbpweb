e719188bda0b00c68586a389999bae0e278267e3
Kevin <chevin_ken@berkeley.edu>
2020-10-25 00:29:37 -0700

    Setting up OCF apphosting for TBP

---
+++ tbpweb/prod.py
@@ -0,0 +1,17 @@
+from .settings import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = [
+    'localhost', 'tbp.apphost.ocf.berkeley.edu'
+]
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_URL = 'https://www.ocf.berkeley.edu/~tbp/static/'
+STATIC_ROOT = '/home/t/tb/tbp/public_html/tbpweb/static'
+
+# Media files (user-uploaded files)
+# https://docs.djangoproject.com/en/2.1/topics/files/
+MEDIA_URL = 'https://www.ocf.berkeley.edu/~tbp/media/'
+MEDIA_ROOT = '/home/t/tb/tbp/public_html/tbpweb/static'
--- tbpweb/wsgi.py
+++ tbpweb/wsgi.py
@@ -11,6 +11,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tbpweb.settings')
+try:
+    MODE = os.environ['MODE'].lower()
+    if (MODE == 'prod'):
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tbpweb.prod')
+    else: os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tbpweb.dev')
+except KeyError:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tbpweb.dev')
 
 application = get_wsgi_application()
