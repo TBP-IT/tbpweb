ff6afcd22f2ca5d031bc01f2d332ad25178c09ee
Oscar Chan <oscar4856@yahoo.com>
2021-09-18 00:53:23 -0700

    Fix Account Auth Views in urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,9 @@
+from re import template
 from django.urls import reverse_lazy
 from django.urls import re_path
-from django.contrib.auth.views import login
-from django.contrib.auth.views import logout
-from django.contrib.auth.views import password_change
-from django.contrib.auth.views import password_change_done
-from django.contrib.auth.views import password_reset
-from django.contrib.auth.views import password_reset_complete
-from django.contrib.auth.views import password_reset_confirm
-from django.contrib.auth.views import password_reset_done
+from django.contrib.auth.views import LoginView, LogoutView, \
+                                      PasswordChangeView, PasswordChangeDoneView, PasswordResetView, \
+                                      PasswordResetCompleteView, PasswordResetConfirmView, PasswordResetDoneView
 
 from accounts.forms import AuthenticationForm
 from accounts.forms import PasswordChangeForm
@@ -18,55 +14,43 @@ from accounts.forms import SetPasswordForm
 app_name = 'accounts'
 
 urlpatterns = [
-    re_path(r'^login/$', login,
-        {'template_name': 'accounts/login.html',
-         'authentication_form': AuthenticationForm,
-         'current_app': app_name},
+    re_path(r'^login/$',
+        LoginView.as_view(template_name='accounts/login.html',
+                          authentication_form=AuthenticationForm),
         name='login'),
-    re_path(r'^logout/$', logout,
+    re_path(r'^logout/$',
         # next_page is used to redirect to the root URL after logout if the
         # requested URL doesn't contain a redirect GET field
-        {'next_page': reverse_lazy('home'),
-         'template_name': 'accounts/logout.html',
-         'current_app': app_name},
+        LogoutView.as_view(next_page=reverse_lazy('home'), template_name='accounts/logout.html'),
         name='logout'),
 
     # Changing password (change, then done):
-    re_path(r'password/change/$', password_change,
-        {'template_name': 'accounts/password_change.html',
-         'post_change_redirect': reverse_lazy('accounts:password-change-done'),
-         'password_change_form': PasswordChangeForm,
-         'current_app': app_name},
+    re_path(r'password/change/$',
+        PasswordChangeView.as_view(template_name='accounts/password_change.html',
+                                   success_url=reverse_lazy('accounts:password-change-done'),
+                                   form_class=PasswordChangeForm),
         name='password-change'),
-    re_path(r'password/change/done/$', password_change_done,
-        {'template_name': 'accounts/password_change_done.html',
-         'current_app': app_name},
+    re_path(r'password/change/done/$',
+        PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'),
         name='password-change-done'),
 
     # Resetting password (reset, then sent, then confirm, then done):
-    re_path(r'password/reset/$', password_reset,
-        {'template_name': 'accounts/password_reset.html',
-         'email_template_name': 'accounts/password_reset_email.html',
-         'password_reset_form': PasswordResetForm,
-         'post_reset_redirect': reverse_lazy('accounts:password-reset-sent'),
-         'current_app': app_name},
+    re_path(r'password/reset/$', PasswordResetView.as_view(template_name='accounts/password_reset.html',
+         email_template_name='accounts/password_reset_email.html',
+         form_class=PasswordResetForm,
+         success_url=reverse_lazy('accounts:password-reset-sent')),
         name='password-reset'),
-    re_path(r'password/reset/sent/$', password_reset_done,
-        {'template_name': 'accounts/password_reset_sent.html',
-         'current_app': app_name},
+    re_path(r'password/reset/sent/$', PasswordResetDoneView.as_view(template_name='accounts/password_reset_sent.html'),
         name='password-reset-sent'),
     # URL regex for uid and token copied from Django source
     re_path((r'^password/reset/(?P<uidb64>[0-9A-Za-z_\-]+)/'
          '(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$'),
-        password_reset_confirm,
-        {'template_name': 'accounts/password_reset_confirm.html',
-         'set_password_form': SetPasswordForm,
-         'post_reset_redirect': reverse_lazy(
-             'accounts:password-reset-complete'),
-         'current_app': app_name},
+        PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html',
+                                         form_class=SetPasswordForm,
+                                         success_url=reverse_lazy(
+                                            'accounts:password-reset-complete')),
         name='password-reset-confirm'),
-    re_path(r'^password/reset/complete/', password_reset_complete,
-        {'template_name': 'accounts/password_reset_complete.html',
-         'current_app': app_name},
+    re_path(r'^password/reset/complete/',
+        PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),
         name='password-reset-complete'),
 ]
