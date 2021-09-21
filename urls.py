4f7da9049d0c34f9705264efa940ae1fb5c1c727
ochan1 <oscar4856@yahoo.com>
2021-09-21 01:04:17 -0700

    Change root urls.py paths + add debug_toolbar path

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.urls import include
-from django.urls import re_path
+from django.urls import path, re_path
+from django.contrib.flatpages import views
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -11,58 +12,63 @@ admin.autodiscover()
 
 
 urlpatterns = [
-    re_path(r'^', include('base.urls')),
-    re_path(r'^accounts/', include('accounts.urls')),
-    re_path(r'^achievements/', include('achievements.urls')),
-    re_path(r'^admin/', admin.site.urls),
-    re_path(r'^alumni/', include('alumni.urls')),
-    re_path(r'^candidates/', include('candidates.urls')),
-    re_path(r'^courses/', include('courses.urls')),
-    re_path(r'^email/', include('emailer.urls')),
-    re_path(r'^events/', include('events.urls')),
-    re_path(r'^exams/', include('exams.urls')),
-    re_path(r'^houses/', include('houses.urls')),
-    re_path(r'^syllabi/', include('syllabi.urls')),
-    re_path(r'^industry/', include('companies.urls')),
-    re_path(r'^mailing-lists/', include('mailing_lists.urls')),
-    re_path(r'^minutes/', include('minutes.urls')),
-    re_path(r'^newsreel/', include('newsreel.urls')),
-    re_path(r'^notifications/', include('notifications.urls')),
-    re_path(r'^past-presidents/', include('past_presidents.urls')),
-    re_path(r'^profile/', include('user_profiles.urls')),
-    re_path(r'^project-reports/', include('project_reports.urls')),
-    re_path(r'^quote-board/', include('quote_board.urls')),
-    re_path(r'^resumes/', include('resumes.urls')),
-    re_path(r'^syllabi/', include('syllabi.urls')),
-    re_path(r'^videos/', include('videos.urls')),
-    re_path(r'^vote/', include('vote.urls')),
+    path('', include('base.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('achievements/', include('achievements.urls')),
+    path('admin/', admin.site.urls),
+    path('alumni/', include('alumni.urls')),
+    path('candidates/', include('candidates.urls')),
+    path('courses/', include('courses.urls')),
+    path('email/', include('emailer.urls')),
+    path('events/', include('events.urls')),
+    path('exams/', include('exams.urls')),
+    path('houses/', include('houses.urls')),
+    path('syllabi/', include('syllabi.urls')),
+    path('industry/', include('companies.urls')),
+    path('mailing-lists/', include('mailing_lists.urls')),
+    path('minutes/', include('minutes.urls')),
+    path('newsreel/', include('newsreel.urls')),
+    path('notifications/', include('notifications.urls')),
+    path('past-presidents/', include('past_presidents.urls')),
+    path('profile/', include('user_profiles.urls')),
+    path('project-reports/', include('project_reports.urls')),
+    path('quote-board/', include('quote_board.urls')),
+    path('resumes/', include('resumes.urls')),
+    path('syllabi/', include('syllabi.urls')),
+    path('videos/', include('videos.urls')),
+    path('vote/', include('vote.urls')),
 ]
 
 # Add flatpages URLs
 urlpatterns += [
-    'django.contrib.flatpages.views',
-    re_path(r'^about/$', 'flatpage', {'url': '/about/'}, name='about'),
-    re_path(r'^about/contact/$', 'flatpage', {'url': '/about/contact/'},
+    path('about/', views.flatpage, {'url': '/about/'}, name='about'),
+    path('about/contact/', views.flatpage, {'url': '/about/contact/'},
         name='contact'),
-    re_path(r'^about/eligibility/$', 'flatpage', {'url': '/about/eligibility/'},
+    path('about/eligibility/', views.flatpage, {'url': '/about/eligibility/'},
         name='eligibility'),
-    re_path(r'^people/committees/$', 'flatpage', {'url': '/people/committees/'},
+    path('people/committees/', views.flatpage, {'url': '/people/committees/'},
         name='committees'),
-    re_path(r'^student-resources/$', 'flatpage', {'url': '/student-resources/'},
+    path('student-resources/', views.flatpage, {'url': '/student-resources/'},
         name='student-resources'),
-    re_path(r'^resume-critique/$', 'flatpage', {'url': '/resume-critique/'},
+    path('resume-critique/', views.flatpage, {'url': '/resume-critique/'},
         name='resume-critique'),
-    re_path(r'^mock-interviews/$', 'flatpage', {'url': '/mock-interviews/'},
+    path('mock-interviews/', views.flatpage, {'url': '/mock-interviews/'},
         name='mock-interviews'),
-    re_path(r'^partnership_program/$', 'flatpage', {'url': '/partnership_program/'},
+    path('partnership_program/', views.flatpage, {'url': '/partnership_program/'},
         name='partnership_program'),
 ]
 
 # Handle page redirects
 urlpatterns += [
-    re_path(r'^indrel/$', RedirectView.as_view(pattern_name='industry')),
+    path('indrel/', RedirectView.as_view(pattern_name='industry')),
 ]
 
 if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    import debug_toolbar
+
+    urlpatterns += [
+        path('__debug__', include(debug_toolbar.urls)),
+    ]
