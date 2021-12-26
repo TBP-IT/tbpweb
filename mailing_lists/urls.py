43901d8e4e1c9222f185e908f8b5dac987af5808
Oscar Chan <oscar4856@yahoo.com>
2021-12-25 21:40:13 -0800

    Remove mailing_lists app that use mailman

--- mailing_lists/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import re_path
-
-from mailing_lists.views import MailingListsListAllView
-
-
-urlpatterns = [
-    re_path(r'^$', MailingListsListAllView.as_view(), name='list'),
-]
