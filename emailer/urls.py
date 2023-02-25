4f739d2c43733756352d563d504fc3858157e029
Oscar Chan <oscar4856@yahoo.com>
2023-02-24 23:03:29 -0800

    Remove all Helpdesk reference

--- emailer/urls.py
+++ emailer/urls.py
@@ -2,12 +2,9 @@ from django.urls import re_path
 
 from emailer.views import CompanyEmailerView
 from emailer.views import EventEmailerView
-from emailer.views import HelpdeskEmailerView
 
 
 urlpatterns = [
-    re_path(r'^helpdesk/$', HelpdeskEmailerView.as_view(),
-        name='helpdesk'),
     re_path(r'^events/(?P<event_pk>\d+)/$', EventEmailerView.as_view(),
         name='event'),
     re_path(r'^industry/$', CompanyEmailerView.as_view(),
