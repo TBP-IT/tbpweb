f96b002b80ef51ea664b015e209346deaf53eb20
Owen Thompson <owent@berkeley.edu>
2023-11-10 13:21:48 -0800

    Allow unicode/emojis in db

--- settings/production.py
+++ settings/production.py
@@ -27,6 +27,7 @@ DATABASES = {
         'NAME': 'tbp',
         'HOST': 'mysql',
         'OPTIONS': {
+            'charset': "utf8mb4",
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
             'read_default_file': "~/.my.cnf",
         },
